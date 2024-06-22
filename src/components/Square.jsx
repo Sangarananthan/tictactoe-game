b68c1870a6ec41c8df16a27d81f4cc88ff71742a
Sangarananthan <sangarsmart16@gmail.com>
2024-06-22 15:56:21 +0530

    X logic was implemented

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,6 @@
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button className="square" type="button">
+    <button className="square" type="button" onClick={onClick}>
       {value}
     </button>
   );
